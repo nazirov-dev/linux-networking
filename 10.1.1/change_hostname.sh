1ea8ecc3a214a731bf582f01839b9d3b003bce28
Abdulaziz Nazirov <nazirovdev@gmail.com>
2025-01-15 10:31:58 +0500

    Push

--- 10.1.1/change_hostname.sh
+++ 10.1.1/change_hostname.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# Hostname ni o'zgartirish
+# Change hostname
+echo "Changing hostname to 'server01'"
 hostnamectl set-hostname server01
 
-# Interfeyslarni yaratish
-for i in {1..19}; do
-    ip link add br-5720f05dd68a.$i type bridge
-    ip addr add 172.16.50.1/24 brd 172.16.50.255 dev br-5720f05dd68a.$i
-    ip link set br-5720f05dd68a.$i up
-done
+# Create a virtual bridge interface (br0)
+echo "Creating virtual bridge interface br0"
+ip link add name br0 type bridge
+ip link set br0 up
+ip addr add 192.168.1.1/24 dev br0
+
+# Create a virtual Ethernet pair (veth0, veth1)
+echo "Creating virtual Ethernet pair veth0 and veth1"
+ip link add veth0 type veth peer name veth1
+
+# Attach veth0 to the bridge
+echo "Attaching veth0 to br0 bridge"
+ip link set veth0 master br0
+
+# Bring up the interfaces
+echo "Bringing up the interfaces"
+ip link set veth0 up
+ip link set veth1 up
+
+# Optionally, assign an IP to veth0 (if required)
+echo "Assigning IP address to veth0"
+ip addr add 192.168.1.2/24 dev veth0
+
+echo "Fake network interfaces created successfully"
