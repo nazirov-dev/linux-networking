ffb0c9a3b552cbc0be6ebc776caf84251e6a8ce0
Abdulaziz Nazirov <nazirovdev@gmail.com>
2025-01-15 11:13:53 +0500

    Push

--- 10.1.1/create_fake_network.sh
+++ 10.1.1/create_fake_network.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-# Change the hostname as root
+# Step 1: Change the hostname to 'server01'
 echo "Changing hostname to 'server01'"
 echo "server01" | sudo tee /etc/hostname
 echo "127.0.0.1   server01" | sudo tee -a /etc/hosts
 
-# Create a virtual bridge interface (br0)
-echo "Creating virtual bridge interface br0"
-sudo ip link add name br0 type bridge
-sudo ip link set br0 up
-sudo ip addr add 192.168.1.1/24 dev br0
+# Step 2: Create 19 virtual interfaces and assign IPs
+# 1. Create the first bridge interface br-5720f05dd68a
+echo "Creating virtual bridge interface br-5720f05dd68a"
+sudo ip link add name br-5720f05dd68a type bridge
+sudo ip addr add 172.16.50.1/24 dev br-5720f05dd68a
+sudo ip link set br-5720f05dd68a up
 
-# Create a virtual Ethernet pair (veth0, veth1)
-echo "Creating virtual Ethernet pair veth0 and veth1"
-sudo ip link add veth0 type veth peer name veth1
+# 2. Create virtual Ethernet pairs (vethX and vethY) and attach them to the bridge
+for i in $(seq 1 19); do
+    # Create virtual Ethernet pair veth$i and veth$((i+1))
+    echo "Creating virtual Ethernet pair veth$i and veth$((i+1))"
+    sudo ip link add veth$i type veth peer name veth$((i+1))
 
-# Attach veth0 to the bridge
-echo "Attaching veth0 to br0 bridge"
-sudo ip link set veth0 master br0
+    # Attach veth$i to br-5720f05dd68a bridge
+    echo "Attaching veth$i to br-5720f05dd68a bridge"
+    sudo ip link set veth$i master br-5720f05dd68a
 
-# Bring up the interfaces
-echo "Bringing up the interfaces"
-sudo ip link set veth0 up
-sudo ip link set veth1 up
+    # Bring up both interfaces
+    echo "Bringing up veth$i and veth$((i+1))"
+    sudo ip link set veth$i up
+    sudo ip link set veth$((i+1)) up
+done
 
-# Optionally, assign an IP to veth0 (if required)
-echo "Assigning IP address to veth0"
-sudo ip addr add 192.168.1.2/24 dev veth0
+# Step 3: Assign IP addresses to the interfaces if needed
+echo "Assigning IP address to veth1"
+sudo ip addr add 172.16.50.2/24 dev veth1
 
 echo "Fake network interfaces created successfully"
