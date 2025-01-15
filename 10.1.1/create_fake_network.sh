5c72ea278c771c55a1f326bb2ba411287794fb04
Abdulaziz Nazirov <nazirovdev@gmail.com>
2025-01-15 11:25:39 +0500

    Push

--- 10.1.1/create_fake_network.sh
+++ 10.1.1/create_fake_network.sh
@@ -6,21 +6,22 @@ echo "server01" | sudo tee /etc/hostname
 echo "127.0.0.1   server01" | sudo tee -a /etc/hosts
 
 # Step 2: Create 17 virtual interfaces and assign IPs
-# 1. Create the first bridge interface br-5720f05dd68a
-echo "Creating virtual bridge interface br-5720f05dd68a"
-sudo ip link add name br-5720f05dd68a type bridge
-sudo ip addr add 172.16.50.1/24 dev br-5720f05dd68a
-sudo ip link set br-5720f05dd68a up
-
-# 2. Create virtual Ethernet pairs (vethX and vethY) and attach them to the bridge
+# 1. Create the first bridge interface with a random name
 for i in $(seq 1 16); do
+    random_string=$(openssl rand -hex 5)
+    bridge_name="br-$random_string"
+    echo "Creating virtual bridge interface $bridge_name"
+    sudo ip link add name $bridge_name type bridge
+    sudo ip addr add 172.16.50.$((i+1))/24 dev $bridge_name
+    sudo ip link set $bridge_name up
+
     # Create virtual Ethernet pair veth$i and veth$((i+1))
     echo "Creating virtual Ethernet pair veth$i and veth$((i+1))"
     sudo ip link add veth$i type veth peer name veth$((i+1))
 
-    # Attach veth$i to br-5720f05dd68a bridge
-    echo "Attaching veth$i to br-5720f05dd68a bridge"
-    sudo ip link set veth$i master br-5720f05dd68a
+    # Attach veth$i to the created bridge
+    echo "Attaching veth$i to $bridge_name bridge"
+    sudo ip link set veth$i master $bridge_name
 
     # Bring up both interfaces
     echo "Bringing up veth$i and veth$((i+1))"
