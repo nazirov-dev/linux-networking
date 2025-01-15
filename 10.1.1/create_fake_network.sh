796fbe8186205edd3e98b0853eb51f9a3e942956
Abdulaziz Nazirov <nazirovdev@gmail.com>
2025-01-15 12:04:00 +0500

    Push

--- 10.1.1/create_fake_network.sh
+++ 10.1.1/create_fake_network.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 # Step 1: Create 19 virtual interfaces with random names
-# 1. Create the first bridge interface br-5720f05dd68a
-echo "Creating virtual bridge interface br-5720f05dd68a"
-sudo ip link add name br-5720f05dd68a type bridge
-sudo ip addr add 172.16.50.1/24 dev br-5720f05dd68a
-sudo ip link set br-5720f05dd68a up
 
 # 2. Create virtual Ethernet pairs (veth{random-string} and veth{random-string}) and attach them to the bridge
-for i in $(seq 1 8); do
+for i in $(seq 1 9); do
     # Generate random strings for interface names using /dev/urandom
     random_veth1="veth$(head /dev/urandom | tr -dc a-f0-9 | head -c 8)"
     random_veth2="veth$(head /dev/urandom | tr -dc a-f0-9 | head -c 8)"
@@ -25,4 +20,9 @@ for i in $(seq 1 8); do
     echo "Bringing up $random_veth1 and $random_veth2"
     sudo ip link set $random_veth1 up
     sudo ip link set $random_veth2 up
+    if [ $i -eq 4 ]; then
+        sudo ip link add name br-5720f05dd68a type bridge
+        sudo ip addr add 172.16.50.1/24 dev br-5720f05dd68a
+        sudo ip link set br-5720f05dd68a up
+    fi
 done
